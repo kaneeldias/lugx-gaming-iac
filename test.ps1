8c23165eaf7ebd07709e5324c55d9235d7e34e5f
kaneeldias <kaneeldias@hotmail.com>
2025-07-26 03:02:39 +0530

    Add deployment and service configuration for order service API

--- test.ps1
+++ test.ps1
@@ -1,10 +1,12 @@
 kubectl delete deployment lugx-gaming-frontend-deployment
 kubectl delete secret lugx-gaming-database-credentials-secret
 kubectl delete deployment lugx-gaming-games-service-api-deployment
+kubectl delete deployment lugx-gaming-order-service-api-deployment
 kubectl delete deployment lugx-gaming-database-deployment
 kubectl delete pvc lugx-gaming-database-pvc
 
 kubectl apply -f .\database\lugx-gaming-database-secret.yml
 kubectl apply -f .\database\lugx-gaming-database.yml
 kubectl apply -f .\lugx-gaming-games-service-api.yml
+kubectl apply -f .\lugx-gaming-order-service-api.yml
 kubectl apply -f .\lugx-gaming-frontend.yml
