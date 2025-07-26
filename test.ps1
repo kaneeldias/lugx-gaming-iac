c9a17adce350d7c1ae27eabcac4f8a3cea985fc8
kaneeldias <kaneeldias@hotmail.com>
2025-07-27 03:22:44 +0530

    Add Ingress configuration and update storage class in PVCs

--- test.ps1
+++ test.ps1
@@ -1,20 +1,22 @@
 # ============ DELETE RESOURCES ================
-kubectl delete deployment lugx-gaming-frontend-deployment
-
-kubectl delete deployment lugx-gaming-database-deployment
-kubectl delete secret lugx-gaming-database-credentials-secret
-kubectl delete pvc lugx-gaming-database-pvc
-
-kubectl delete deployment lugx-gaming-clickhouse-deployment
-kubectl delete secret lugx-gaming-clickhouse-credentials-secret
-kubectl delete configmap lugx-gaming-clickhouse-init-configmap
-kubectl delete pvc lugx-gaming-clickhouse-pvc
-
-kubectl delete deployment lugx-gaming-games-service-api-deployment
-kubectl delete deployment lugx-gaming-order-service-api-deployment
-kubectl delete deployment lugx-gaming-analytics-service-api-deployment
+#kubectl delete deployment lugx-gaming-frontend-deployment
+#
+#kubectl delete deployment lugx-gaming-database-deployment
+#kubectl delete secret lugx-gaming-database-credentials-secret
+#kubectl delete pvc lugx-gaming-database-pvc
+#
+#kubectl delete deployment lugx-gaming-clickhouse-deployment
+#kubectl delete secret lugx-gaming-clickhouse-credentials-secret
+#kubectl delete configmap lugx-gaming-clickhouse-init-configmap
+#kubectl delete pvc lugx-gaming-clickhouse-pvc
+#
+#kubectl delete deployment lugx-gaming-games-service-api-deployment
+#kubectl delete deployment lugx-gaming-order-service-api-deployment
+#kubectl delete deployment lugx-gaming-analytics-service-api-deployment
 
 # ============ CREATE RESOURCES ================
+kubectl apply -f .\storage\storage.yml
+
 kubectl apply -f .\database\lugx-gaming-database-secret.yml
 kubectl apply -f .\database\lugx-gaming-database.yml
 
@@ -29,3 +31,6 @@ kubectl apply -f .\lugx-gaming-analytics-service-api.yml
 kubectl apply -f .\lugx-gaming-frontend.yml
 
 kubectl apply -f .\datadog\datadog-agent.yml
+
+kubectl apply -f .\ingress\ingress-class.yml
+kubectl apply -f .\ingress\ingress.yml
